da8ea165b26f78c7cc23aec649dfd63f0000b740
yattinda <yattinda@gmail.com>
2021-06-12 23:18:02 +0900

    Add random position

--- style.js
+++ style.js
@@ -1,6 +1,13 @@
 AFRAME.registerComponent('rand_position_num', {
+  var min = 10;
+  var xmin = -50;
+  var max = 50;
   init: function () {
     this.el.addEventListener('click', function (evt) {
+      var x = Math.floor( Math.random() * (max + 1 - xmin) ) + xmin ;
+      var y = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+      var z = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+      this.setAttribute('material', 'position', 'x y z');
     });
   }
 });
