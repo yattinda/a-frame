4fa2f16fe7494a120c019d182a349fd962618d21
yattinda <yattinda@gmail.com>
2021-06-14 14:58:19 +0900

    Test

--- style.js
+++ style.js
@@ -1,8 +1,8 @@
-
-AFRAME.registerComponent('delete', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (event) {
-      target.style.opacity = "0";
-    }, false);
-  }
-});
+// 
+// AFRAME.registerComponent('delete', {
+//   init: function () {
+//     this.el.addEventListener('mouseenter', function (event) {
+//       target.style.opacity = "0";
+//     }, false);
+//   }
+// });
