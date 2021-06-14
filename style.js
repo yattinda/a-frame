eb804c2a0c04d967ebb177b56062bf2a1c10950d
yattinda <yattinda@gmail.com>
2021-06-14 14:52:36 +0900

    Test

--- style.js
+++ style.js
@@ -1,14 +1,8 @@
-// var min = 10;
-// var xmin = -50;
-// var max = 50;
-// AFRAME.registerComponent('rand_position', {
-//   init: function () {
-//     this.el.addEventListener('click', function (evt) {
-//       var x = (Math.floor( Math.random() * (max + 1 - xmin) ) + xmin).toString() ;
-//       var y = (Math.floor( Math.random() * (max + 1 - min) ) + min).toString() ;
-//       var z = (Math.floor( Math.random() * (max + 1 - min) ) + min).toString() ;
-//       console.log(x + " " + y + " " + z);
-//       this.setAttribute('material', 'position', x + " " + y + " " + z);
-//     });
-//   }
-// });
+
+AFRAME.registerComponent('delete', {
+  init: function () {
+    this.el.addEventListener('mouseenter', function (event) {
+      target.style.opacity = "0";
+    }, false);
+  }
+});
