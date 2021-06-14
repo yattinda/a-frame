6d4e318ad9d601f3d9fede19e4f61adea5801106
yattinda <yattinda@gmail.com>
2021-06-14 13:21:37 +0900

    Declear var

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
+var min = 10;
+var xmin = -50;
+var max = 50;
 AFRAME.registerComponent('rand_position', {
-  var min = 10;
-  var xmin = -50;
-  var max = 50;
   init: function () {
     this.el.addEventListener('click', function (evt) {
       var x = (Math.floor( Math.random() * (max + 1 - xmin) ) + xmin).toString() ;
