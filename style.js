76ad9afbd3b2cf7258ccd9af459ad193d7ae2c11
yattinda <yattinda@gmail.com>
2021-06-14 15:25:19 +0900

    Fix

--- style.js
+++ style.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent('delete', {
   init: function () {
     this.el.addEventListener('mouseenter', function (event) {
       var me = document.getElementById(event.target.id);
-      me.parentNode.removeChild(me);
+      me.removeChild(me);
     });
   }
 });
